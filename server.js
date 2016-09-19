4ed94fa0fcb30b6762a26de4a189592568b9aa69
Robert <robert@tooconnect.nl>
2016-09-19 12:46:57 +0200

    Add paramete completed filter

--- server.js
+++ server.js
@@ -36,44 +36,61 @@ app.get("/", function (req, res) {
     res.send("Todo API root")
 })
 
-// get todos
+// get todos?completed
 app.get("/todos", function (req, res) {
-        res.json(todos)
+        var queryParams = req.query;
+        var filterdTodos = todos;
+    var queryCompleted = queryParams.completed;
+    //console.log("queryCompleted: " + typeof queryCompleted);
+
+        if (queryParams.hasOwnProperty('completed')){
+            queryCompleted = queryParams.completed;
+            if(queryCompleted==='true'){
+                filterdTodos = _.where(filterdTodos,{completed:true})
+            }else if(queryCompleted==='false'){
+                filterdTodos = _.where(filterdTodos,{completed:false})
+            }
+
+        }
+
+
+
+        res.json(filterdTodos)
     }
 )
 
 // get specific id  todos/:id
 app.get("/todos/:id", function (req, res) {
     var todoId = parseInt(req.params.id);
-    var matchedToDo = _.findWhere(todos,{id: todoId});
+    var matchedToDo = _.findWhere(todos, {id: todoId});
     /*
 
-    var todoFound = false;
+     var todoFound = false;
 
-    console.log("Asking for todo with id of " + todoId);
-    for (i = 0; i < todos.length; i++) {
-        console.log("todosID[" + i + "].id = " + todos[i].id);
-        // res.send("loop " + i);
-        // res.json(todos[i]);
-        if (todos[i].id === todoId) {
-            res.json(todos[i]);
-            todoFound = true;
-            break;
-        }
-    }
+     console.log("Asking for todo with id of " + todoId);
+     for (i = 0; i < todos.length; i++) {
+     console.log("todosID[" + i + "].id = " + todos[i].id);
+     // res.send("loop " + i);
+     // res.json(todos[i]);
+     if (todos[i].id === todoId) {
+     res.json(todos[i]);
+     todoFound = true;
+     break;
+     }
+     }
 
      */
     if (matchedToDo) {
         res.json(matchedToDo);
-    }else{
+    } else {
         res.sendStatus(404); //.send("Todo " + todoID +" not found");
     }
 
 })
 
 app.post('/todos', function (req, res) {
-    var body= _.pick(req.body,'description', 'completed');
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length===0){
+    var body = _.pick(req.body, 'description', 'completed');
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
         return res.sendStatus(400);
     }
     body.id = todoNextId++
@@ -81,12 +98,12 @@ app.post('/todos', function (req, res) {
     /*
 
      console.log('description: ' + body.description);
-    var todo = {
-        id: todoNextId,
-        description: body.description,
-        completed: body.completed,
-    }
-   todoNextId++
+     var todo = {
+     id: todoNextId,
+     description: body.description,
+     completed: body.completed,
+     }
+     todoNextId++
      */
     todos.push(body);
     res.json(body);
@@ -96,51 +113,51 @@ app.post('/todos', function (req, res) {
 //DELETE
 app.delete("/todos/:id", function (req, res) {
     var todoId = parseInt(req.params.id);
-    var matchedToDo = _.findWhere(todos,{id: todoId});
+    var matchedToDo = _.findWhere(todos, {id: todoId});
 
     if (matchedToDo) {
-       todos = _.without(todos,matchedToDo)
+        todos = _.without(todos, matchedToDo)
         res.json(todos);
-    }else{
+    } else {
         res.status(404).send(); //json({"Todo: " + todoId + " not found"});
-       // res.sendStatus(404);//.json("Todo " + todoID +" not found"); ID moet zijn Id
+        // res.sendStatus(404);//.json("Todo " + todoID +" not found"); ID moet zijn Id
     }
 
 })
 
-app.put("/todos/:id",function (req,res) {
+app.put("/todos/:id", function (req, res) {
     var todoId = parseInt(req.params.id);
-    var matchedToDo = _.findWhere(todos,{id: todoId});
-    var body= _.pick(req.body,'description', 'completed');
+    var matchedToDo = _.findWhere(todos, {id: todoId});
+    var body = _.pick(req.body, 'description', 'completed');
     var validAttributes = {};
 
     if (!matchedToDo) {
-       return res.status(404).send(); //({"Todo: " + todoId + " not found"});
+        return res.status(404).send(); //({"Todo: " + todoId + " not found"});
     }
 
 
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed) ){
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
         validAttributes.completed = body.completed;
     }
-    else if (body.hasOwnProperty('completed') ){
+    else if (body.hasOwnProperty('completed')) {
         return res.status(400).send();//.json({"error":"errormessage","var":"value"});
-    }else{
+    } else {
 
     }
 
 
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length>0 ){
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
         validAttributes.description = body.description;
     }
-    else if (body.hasOwnProperty('description') ){
+    else if (body.hasOwnProperty('description')) {
         return res.status(400).send();
-    }else{
+    } else {
 
     }
 
     _.extend(matchedToDo, validAttributes);
 
-res.send(matchedToDo);
+    res.send(matchedToDo);
 
 })
 
---
+++ variable-playground.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Robert on 19-9-2016.
+ */
+
+var person = {
+    name:"Robert Collewijn",
+    age: 46
+}
+
+function updatePerson(obj) {
+
+}
