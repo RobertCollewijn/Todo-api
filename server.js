24ecffc220343e1dd144415b34bf455654dd7701
Robert <robert@tooconnect.nl>
2016-09-20 15:06:44 +0200

    hashed and salt password

--- server.js
+++ server.js
@@ -139,10 +139,10 @@ app.post('/users', function (req, res) {
     //    console.log("Everything is synced");
 
         db.user.create(body).then(function (user) {
-            res.json(user.toJSON());
+            res.json(user.toPublicJSON());
         }).catch(function (e) {
             res.status(400).json(e)
-        })
+        });
 
   //  })
 });
