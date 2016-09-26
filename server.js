573bc2f5509a04a1fa896dae30673958ed94d8d2
Robert <robert@tooconnect.nl>
2016-09-26 10:52:39 +0200

    logout

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.delete("/todos/:id", middleware.requireAuthentication, function (req, res) {
     db.todo.destroy({
         where: {
             id: todoId,
-            userId:req.user.get('id')// add user req.get.
+            userId: req.user.get('id')// add user req.get.
         }
     }).then(function (rowsDeleted) {
         if (rowsDeleted === 0) {
@@ -134,9 +134,9 @@ app.put("/todos/:id", middleware.requireAuthentication, function (req, res) {
     }
 
     db.todo.findOne({
-        where:{
-            id:todoId,
-            userId:req.user.get('id')
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
         }
     }).then(function (todo) {
         if (todo) {
@@ -171,20 +171,40 @@ app.post('/users', function (req, res) {
 // POST /user/login
 app.post('/users/login', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
+    var userInstance;
 
     db.user.authenticate(body).then(function (user) {
         var token = user.generateToken('authentication');
-        if (token) {
-            res.header('Auth', token).json(user.toPublicJSON());
-        } else {
-            res.status(401).send();
-        }
-        ;
+        userInstance = user;
+        return db.token.create({
+            token: token
+        });
+
+        /*
+         if (token) {
+         res.header('Auth', token).json(user.toPublicJSON());
+         } else {
+         res.status(401).send();
+         }
+         */
+    }).then(function (tokenInstance) {
+        res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
     }).catch(function (e) {
         res.status(401).send();
     })
 
 });
+
+//DELETE // users/logoi
+
+app.delete('/users/login', middleware.requireAuthentication, function (req,res) {
+    req.token.destroy().then(function () {
+        res.status(204).send();
+    }).catch( function () {
+        res.status(500).send();
+    })
+    
+})
 
 db.sequelize.sync({force: true}).then(function () {
         app.listen(PORT, function () {
